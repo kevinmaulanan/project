0110e2473055b70396988205088b1bd869d06f6b
kevinmaulanan <kevinmaulana2703@gmail.com>
2020-03-07 04:35:27 -0500

    CRUD Category with Tabel relation

--- src/controller/category.js
+++ src/controller/category.js
@@ -33,49 +33,62 @@ const getAllCategory = async (req, res) => {
 
 const createCategory = async (req, res) => {
     const { category_detail, id_category } = req.body
-    const data = await processCategory.create(category_detail, id_category)
-    if (data) {
-        res.send({
-            success: true,
-            data
-        })
-    } else {
-        res.send({
-            success: false,
-            msg: 'error'
-        })
+    const { success, message } = await processCategory.create(category_detail, id_category)
+    try {
+        if (success) {
+            res.send({
+                success: true,
+                message
+            })
+        } else {
+            res.send({
+                success: false,
+                message
+            })
+        }
+    } catch (error) {
+        res.send({ success: false, msg: message })
     }
 }
 
 const updateCategory = async (req, res) => {
     const { id, category_detail, id_category } = req.body
-    const data = await processCategory.update(id, category_detail, id_category)
-    if (data) {
-        res.send({
-            success: true,
-            data
-        })
-    } else {
-        res.send({
-            success: false,
-            msg: 'error'
-        })
+    const { success, message } = await processCategory.update(id, category_detail, id_category)
+    console.log(success)
+    try {
+        if (success) {
+            res.send({
+                success: true,
+                message: 'Succes Update Data Category'
+            })
+        } else {
+            res.send({
+                success: false,
+                message
+            })
+        }
+    } catch (error) {
+        res.send({ success: false, message })
     }
 }
 
 const deleteCategory = async (req, res) => {
     const { id } = req.body
-    const data = await processCategory.delete(id)
-    if (data) {
-        res.send({
-            success: true,
-            data
-        })
-    } else {
-        res.send({
-            success: false,
-            msg: 'error'
-        })
+    const { success, message } = await processCategory.delete(id)
+    try {
+        if (success) {
+            res.send({
+                success: true,
+                message
+            })
+        } else {
+            res.send({
+                success: false,
+                message
+            })
+        }
+    } catch (error) {
+        res.send({ success: false, message })
     }
 }
 
